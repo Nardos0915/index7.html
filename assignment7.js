f665b98bf8fac5b551a4ec221e1201d7dbdbedee
Nardos0915 <nardosleges0915@gmmail.com>
2024-11-11 11:41:35 +0200

    second commit

--- assignment7.js
+++ assignment7.js
@@ -1,3 +1,4 @@
+// Destructing colors
 const colors=['red','green','blue']
 
 const[a,b,c]=colors
@@ -6,12 +7,16 @@ console.log(a)
 console.log(b)
 console.log(c) 
 
+//Destructing  properties 
 
 const person={name:'Alice',age:25,city:'Wonderland'}
 const{name,age,city}=person;
 console.log(name)
 console.log(age)
 console.log(city)
+ 
+
+//Destructing examples
 
 const settings={theme:'dark',language:'en'};
 const{theme,language,mode='light'}=settings;
